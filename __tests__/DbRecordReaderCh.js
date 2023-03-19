ced59f34aa952aca4d8159751de3c75480fe0514
Dmitry Ovsyanko <do_@pochta.ru>
2023-03-19 20:55:19 +0300

    DbLangCh +parseType, getStream +[Symbol.for ('columns')]

--- __tests__/DbRecordReaderCh.js
+++ __tests__/DbRecordReaderCh.js
@@ -1,19 +1,23 @@
+const {DbColumn} = require ('doix-db')
+const {DbLangCh} = require ('..')
 const DbRecordReaderCh = require ('../lib/DbRecordReaderCh.js')
 
 test ('basic', async () => {
 
-	const s = new DbRecordReaderCh ({})
-	
+	const s = new DbRecordReaderCh ({lang: new DbLangCh ()})
+
 	const a = []
 	
 	s.on ('data', r => a.push (r))
 
 	s.write ('["id"]\n')
-	s.write ('["Int"]\n')
+	s.write ('["Int32"]\n')
 	s.write ('["1"]')
 	s.write ('\n["2"]')
 	s.end ()
-		
+
 	expect (a).toStrictEqual ([{id: '1'},{id: '2'}])
 	
+	expect (s [Symbol.for ('columns')]).toStrictEqual ([new DbColumn({name: 'id', type: 'Int32'})])
+
 })
---
+++ __tests__/parseType.js
@@ -0,0 +1,11 @@
+const {DbColumn} = require ('doix-db')
+const {DbLangCh} = require ('..'), lang = new DbLangCh ()
+
+test ('parseType', () => {
+
+	expect (lang.parseType ('Int32')).toStrictEqual (new DbColumn({type: 'Int32'}))
+	expect (lang.parseType ('FixedString(1)')).toStrictEqual (new DbColumn({type: 'FixedString', size: 1}))
+	expect (lang.parseType ('Nullable(Int32)')).toStrictEqual (new DbColumn({type: 'Int32', nullable: true}))
+	expect (lang.parseType ('Nullable( Decimal(10, 2))')).toStrictEqual (new DbColumn({type: 'Decimal', nullable: true, size: 10, scale: 2}))
+
+})
