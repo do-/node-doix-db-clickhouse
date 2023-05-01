15b07b3f1f9b10f34f3fe2031647e8eb3b8b9ca6
Dmitry Ovsyanko <do_@pochta.ru>
2023-05-01 10:44:02 +0300

    DbClientCh +setSession

---
+++ __tests__/setSession.js
@@ -0,0 +1,34 @@
+const MockJob = require ('./lib/MockJob.js'), job = new MockJob ()
+const {DbClientCh, DbPoolCh} = require ('..')
+
+const pool = new DbPoolCh ({
+	url: process.env.CONNECTION_STRING,
+})
+
+test ('basic', async () => {
+	
+	try {
+	
+		await pool.setProxy (job, 'db')
+				
+		await job.db.setSession ()
+
+		expect (job.db.url).toMatch ('&session_id=' + job.db.uuid)
+		
+		await job.db.setSession (null)
+
+		expect (job.db.url).not.toMatch ('session_id')
+
+		await job.db.setSession ('abc', 123)
+
+		expect (job.db.url).toMatch ('&session_id=abc')
+		expect (job.db.url).toMatch ('&session_timeout=123')
+
+	}
+	finally {
+
+		await job.db.release ()
+
+	}
+	
+})
