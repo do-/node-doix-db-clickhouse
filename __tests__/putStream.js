1ceffce91b49786c683d52bddb05a2e5debf82d0
Dmitry Ovsyanko <do_@pochta.ru>
2023-11-05 21:14:48 +0300

    putStream tests++

--- __tests__/putStream.js
+++ __tests__/putStream.js
@@ -54,23 +54,27 @@ test ('basic', async () => {
 		await db.do (`CREATE DATABASE ${dbName}`)
 		db.database = dbName
 
-		await db.do ('CREATE TEMPORARY TABLE _ (id UInt8, name String)')
+		await db.do ('CREATE TEMPORARY TABLE _ (id UInt8, name Nullable(String), dt Nullable(Date), amount Nullable(Decimal(10,2)))')
 
-		const os = await db.putStream ('_', ['id', 'name'])
+		const os = await db.putStream ('_', ['id', 'name', 'dt', 'amount'])
 
 		await new Promise ((ok, fail) => {
 
 			os.on ('error', fail)
 			os.on ('complete', ok)
 			
-			os.write ('1,"Name One"\n')
+			os.write ('1,"Name ""One""",1970-01-01,3.72\n')
+			os.write ('2,,,\n')
 			os.end ()
 	
 		})
 
 		const a = await db.getArray ('SELECT *  FROM _')
 
-		expect (a).toStrictEqual ([{id: 1, name: 'Name One'}])		
+		expect (a).toStrictEqual ([
+			{id: 1, name: 'Name "One"', dt: '1970-01-01', amount: 3.72},
+			{id: 2, name: null, dt: null, amount: null},
+		])		
 
 		await db.do (`DROP DATABASE ${dbName}`)
 
